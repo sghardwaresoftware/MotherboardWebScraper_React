b460bde8bcf573d39d6aededa5817e9afdc2d496
ong-kok-peng <darudestorm77@gmail.com>
2026-06-07 02:49:54 +0800

    created react barebones

---
+++ mobo-web-scraper-react/src/App.tsx
@@ -0,0 +1,15 @@
+//import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import SearchScrapeMobo from './components/SearchScrapeMobo'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <SearchScrapeMobo />
+    </>
+  )
+}
+
+export default App
