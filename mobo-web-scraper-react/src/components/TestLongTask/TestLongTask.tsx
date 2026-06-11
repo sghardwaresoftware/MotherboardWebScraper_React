bf7170cafc43f249b8ee2155958d8a010068ed93
ong-kok-peng <darudestorm77@gmail.com>
2026-06-11 17:04:39 +0800

    redone the mobo scrape test component

--- mobo-web-scraper-react/src/components/TestLongTask/TestLongTask.tsx
+++ mobo-web-scraper-react/src/components/TestLongTask/TestLongTask.tsx
@@ -1,21 +1,24 @@
 import { useState, useEffect, useRef } from 'react'
 
 function TestLongTask() {
-    const [progress, setProgress] = useState(0);
+    const [progress, setProgress] = useState<number>(0);
+    const [itemIndex, setItemIndex] = useState<number>(0);
+    const [taskCompleted, setTaskCompleted] = useState<boolean>(false);
 
-    const progressTaskRef = useRef<number | null>(null);
+    const progressBarRef = useRef<number | null>(null);
+    const abortControllerRef = useRef<AbortController | null>(null);
     const statusRef = useRef<string>("idle");
     const dataRef = useRef<string>("");
-    const itemIndexRef = useRef<number>(0);
+    const successItems = useRef<number>(0);
 
     const longTaskTimeoutWait = 20000; // in milliseconds
     const progressBarInterval = (longTaskTimeoutWait + 1000) / 100; // add 1 second to ensure progress bar is not 100% if timeout
     const items = ["item1", "item2", "item3"];
 
     async function runLongTask() {
-        const controller = new AbortController();
-        const timeoutId = setTimeout(() => controller.abort('Timeout reached'), longTaskTimeoutWait); 
-        statusRef.current = "running";
+        abortControllerRef.current = new AbortController();
+        const timeoutId = setTimeout(() => abortControllerRef.current!.abort('timeoutReached'), longTaskTimeoutWait); 
+        statusRef.current = "itemRunning";
 
         try {
             const response = await fetch('https://0e5b84ad-faf7-4fa3-9d6d-141325be66ab.mock.pstmn.io/delay20secs', {
@@ -24,22 +27,20 @@ function TestLongTask() {
                     'Content-Type': 'application/json',
                     'x-mock-response-delay': '18000' // make sure actual task finishes max 2 seconds before timeout wait
                 },
-                signal: controller.signal
+                signal: abortControllerRef.current.signal
             });
 
             if (!response.ok) throw new Error('Fetch failed');
             
             const result = await response.json();
             dataRef.current = JSON.stringify(result);
-            statusRef.current = "success";
+            statusRef.current = "itemSuccess";
             
         } catch (error: any) {
-            if (error.name === 'AbortError' || error === 'Timeout reached') {
-                statusRef.current = "timedOut";
-            } 
-            else {
-                statusRef.current = "fetchFailed";
-            }
+            if (error === 'timeoutReached') statusRef.current = "itemTimedOut";
+            else if (error === 'taskAbortedReached') statusRef.current = "taskAborted";
+            else statusRef.current = "itemFetchFailed";
+
         } finally {
             clearTimeout(timeoutId);
             setProgress(0);
@@ -47,48 +48,56 @@ function TestLongTask() {
     }
 
     function abortScraping() {
-        statusRef.current = "aborted";
+        if (abortControllerRef.current) abortControllerRef.current.abort('taskAbortedReached');
     }
 
     useEffect(() => {
-        if (!progressTaskRef.current) {
-            progressTaskRef.current = setInterval(() => {
+        if (!progressBarRef.current) {
+            progressBarRef.current = setInterval(() => {
                 setProgress((prev) => (prev < 99 ? prev + 1 : prev));
             }, progressBarInterval);
 
             runLongTask();
         }
-        if (progressTaskRef.current && !(statusRef.current == "idle" || statusRef.current == "running")) {
-            clearInterval(progressTaskRef.current);
-            progressTaskRef.current = null;
-            statusRef.current = "idle";
+        if (progressBarRef.current && !(statusRef.current === "idle" || statusRef.current === "itemRunning")) {
+            clearInterval(progressBarRef.current);
+            progressBarRef.current = null;
+            abortControllerRef.current = null;
 
+            if (statusRef.current === "itemSuccess") successItems.current ++;
 
+            setTimeout(() => {
+                if (statusRef.current !== "taskAborted" && itemIndex < items.length - 1) setItemIndex(prevIndex => prevIndex + 1);
+                else if (statusRef.current === "taskAborted" || itemIndex === items.length - 1) { statusRef.current = "taskCompleted"; setTaskCompleted(true) };
+            }, 1000);
+            
         }
-        if (progressTaskRef.current && progress < 99) return;
+        if (progressBarRef.current && progress < 99) return;
 
         return () => {
-            if (progressTaskRef.current) {
-                clearInterval(progressTaskRef.current);
-                progressTaskRef.current = null;
+            if (progressBarRef.current) {
+                clearInterval(progressBarRef.current);
+                progressBarRef.current = null;
+                abortControllerRef.current = null;
             }
         }
-    }, [ progress ]);
+    }, [ progress, itemIndex ]);
 
     let statusText = ""
-    if (statusRef.current == "idle") statusText = "Idle";
-    else if (statusRef.current == "running") statusText = `(${items[itemIndexRef.current]}) Long task in progress...`;
-    else if (statusRef.current == "success") statusText = `Long task finished successfully! Response data: {${dataRef.current}}`;
-    else if (statusRef.current == "timedOut") statusText = "Long task timed out after 20 seconds";
-    else if (statusRef.current == "aborted") statusText = "Long task is aborted";
-    else if (statusRef.current == "fetchFailed") statusText = "Long task failed with error(s)";
+    if (statusRef.current === "idle") statusText = "Idle";
+    else if (statusRef.current === "itemRunning") statusText = `(${items[itemIndex]}) Long task in progress...`;
+    else if (statusRef.current === "itemSuccess") statusText = `(${items[itemIndex]}) Long task completed successfully! Response data: {${dataRef.current}}`;
+    else if (statusRef.current === "itemTimedOut") statusText = `(${items[itemIndex]}) Long task timed out after 20 seconds`;
+    else if (statusRef.current === "taskAborted") statusText = "Long task is aborted";
+    else if (statusRef.current === "itemFetchFailed") statusText = `(${items[itemIndex]}) Long task failed with error(s)`;
+    else if (statusRef.current === "taskCompleted") statusText = `All long tasks have comepleted with ${successItems.current} successful item(s)!`
 
     return (
         <>
             <div className="subContainer">
                 <div className="input-group">
                     <label className="input-group-text col-10">{statusText}</label>
-                    <button onClick={abortScraping} className="btn btn-secondary col-2">Stop Scraping</button>
+                    <button onClick={abortScraping} className="btn btn-secondary col-2" disabled={taskCompleted}>Stop Scraping</button>
                 </div>
                 
                 <div className="progress mt-2">
